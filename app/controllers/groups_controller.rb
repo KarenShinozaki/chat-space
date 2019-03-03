6d24419a3ce6ed905b84c054c4d0b09a4055d754
Karen0731 <karen.s0731s@gmail.com>
2019-03-03 16:32:42 +0900

    deleted view of current_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,12 +6,12 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
     @user = User.where.not(id: current_user)
   end
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -34,7 +34,7 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name, user_ids: [], id: current_user.id)
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def set_group
